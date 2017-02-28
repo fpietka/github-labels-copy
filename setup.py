d35330e9949833b37bfd56007e61ba65c63d6fd9
François Pietka <francois@pietka.fr>
2017-02-28 07:38:11 +0100

    Add more python3 versions compatible

--- setup.py
+++ setup.py
@@ -28,6 +28,8 @@ setup(name='githublabelscopy',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4'
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6'
       ],
       zip_safe=True)
